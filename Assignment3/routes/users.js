5bab1ceb15a0154baef2cefd747dcd54b95b9cdf
subuxd <103200387+subuxd@users.noreply.github.com>
2023-07-03 16:36:04 +0530

    Create users.js

---
+++ Assignment3/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const path = require('path');
+
+router.get('/users', (req,res,next) => {
+    res.sendFile(path.join(__dirname, "../", 'views', 'users.html'));
+})
+
+module.exports=router;
